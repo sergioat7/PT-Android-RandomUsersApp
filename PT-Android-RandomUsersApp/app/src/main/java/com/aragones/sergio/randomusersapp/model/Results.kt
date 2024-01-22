9966534bbf3299e126d45da7bdf841195e3e859b
Sergio Aragonés <sergioat7@gmail.com>
2024-01-22 20:01:47 +0100

    Implement Moshi

--- PT-Android-RandomUsersApp/app/src/main/java/com/aragones/sergio/randomusersapp/model/Results.kt
+++ PT-Android-RandomUsersApp/app/src/main/java/com/aragones/sergio/randomusersapp/model/Results.kt
@@ -1,10 +1,14 @@
 package com.aragones.sergio.randomusersapp.model
 
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
 data class ResultsRaw(
     val results: List<UserRaw>,
     val info: InfoRaw
 )
 
+@JsonClass(generateAdapter = true)
 data class UserRaw(
     val gender: String,
     val name: NameRaw,
@@ -15,28 +19,34 @@ data class UserRaw(
     val picture: PictureRaw
 )
 
+@JsonClass(generateAdapter = true)
 data class NameRaw(
     val first: String,
     val last: String
 )
 
+@JsonClass(generateAdapter = true)
 data class LocationRaw(
     val coordinates: CoordinatesRaw
 )
 
+@JsonClass(generateAdapter = true)
 data class CoordinatesRaw(
     val latitude: String,
     val longitude: String
 )
 
+@JsonClass(generateAdapter = true)
 data class RegisteredRaw(
     val date: String
 )
 
+@JsonClass(generateAdapter = true)
 data class PictureRaw(
     val large: String
 )
 
+@JsonClass(generateAdapter = true)
 data class InfoRaw(
     val results: Int,
     val page: Int
